e7eec39dfe9508851232c53e8cff84e055f5b614
Anthony Pipkin <a.pipkin@yahoo.com>
2013-01-05 16:51:50 -0500

    Remove true privates. Create API documentation.

--- src/gallery-ordered-hash/js/gallery-ordered-hash.js
+++ src/gallery-ordered-hash/js/gallery-ordered-hash.js
@@ -1,195 +1,348 @@
-Y.OrderedHash = function() {
-    var _arr = [],
-        _obj = {},
-        _keyExists = function (key) {
-            return (_obj[key] !== undefined);
-        },
+/**
+@author apipkin
+@class Y.OrderedHash
+@module gallery-ordered-hash
+**/
+var YArray = Y.Array,
+    YObject_owns = Y.Object.owns;
 
-        _getKey = function(obj) {
-            var p;
 
-            for (p in obj) {
-                if (obj.hasOwnProperty(p)) {
-                    return p;
+function OrderedHash () {
+    /**
+    @protected
+    @property _arr
+    @type {Array}
+    **/
+    this._arr = [];
+
+    /**
+    @protected
+    @property _obj
+    @type {Object}
+    **/
+    this._obj = {};
+
+    // constructor
+    var objs, i, l, k;
+
+    if (arguments.length > 0) {
+        // loop through array of pairs
+        if (Y.Lang.isArray(arguments[0])) {
+            objs = arguments[0];
+        } else {
+            objs = arguments;
+        }
+
+        // loop through objs
+        for (i=0,l=objs.length; i<l; i++) {
+            k = null;
+            for (k in objs[i]) {
+                if (objs[i].hasOwnProperty(k)){
+                    this._arr.push(k);
+                    this._obj[k] = objs[i][k];
+                    break;
                 }
             }
-        },
+        }
+    }
+    // -- constructor
+}
 
-        _hash = {
+OrderedHash.prototype = {
 
-            push : function() {
-                var i = 0,
-                    l = arguments.length,
-                    key,
-                    value;
+    /**
+    @public
+    @method push
+    @chainable
+    @returns this
+    **/
+    push : function() {
+        var _arr = this._arr,
+            _obj = this._obj,
+            i = 0,
+            l = arguments.length,
+            key,
+            value;
 
-                for (i = 0; i < l; i++) {
-                    value = arguments[i];
+        for (i = 0; i < l; i++) {
+            value = arguments[i];
 
-                    key = _getKey(value);
-                    if (key) {
-                        if (_keyExists(key)) {
-                            _arr = _arr.splice(_arr.indexOf(key),1);
-                        }
-                        _arr.push(key);
-                        _obj[key] = value[key];
-                    }
+            key = OrderedHash.getKey(value);
+            if (key) {
+                if (Y.Object.owns(_obj, key)) {
+                    _arr = _arr.splice(_arr.indexOf(key),1);
                 }
+                _arr.push(key);
+                _obj[key] = value[key];
+            }
+        }
 
-                return _hash;
-            },
+        return this;
+    },
 
-            pop : function() {
-                var key = _arr.pop(),
-                    val = {};
+    /**
+    @public
+    @method pop
+    @param {Boolean} [asObject] returns a key:value pair if true
+    @returns {Mixed} Last element in the stack
+    **/
+    pop : function(asObject) {
+        var _obj = this._obj,
+            key = this._arr.pop(),
+            val = {};
 
-                if (_obj[key]) {
-                    val = _obj[key];
-                    delete _obj[key];
+        if (_obj[key]) {
+            val = _obj[key];
+            delete _obj[key];
+        }
+
+        return (asObject) ? {key: val} : val;
+    },
+
+    /**
+    @public
+    @method shift
+    @param {Boolean} [asObject] returns a key:value pair if true
+    @returns {Mixed} First element in the stack
+    **/
+    shift : function(asObject) {
+        var _obj = this._obj,
+            key = this._arr.shift(),
+            val = {};
+
+        if (_obj[key]) {
+            val = _obj[key];
+            delete _obj[key];
+        }
+
+        return (asObject) ? {key: val} : val;
+    },
+
+    /**
+    @public
+    @method push
+    @chainable
+    @returns this
+    **/
+    unshift : function() {
+        var _arr = this._arr,
+            _obj = this._obj,
+            i = 0,
+            l = arguments.length,
+            key,
+            value;
+
+        for (i = 0; i < l; i++) {
+            value = arguments[i];
+
+            key = OrderedHash.getKey(value);
+
+            if (key) {
+                if (Y.Object.owns(_obj, key)) {
+                    _arr = _arr.splice(_arr.indexOf(key),1);
                 }
+                _arr.unshift(key);
+                _obj[key] = value[key];
+            }
+        }
 
-                return val;
-            },
+        return this;
+    },
 
-            shift : function() {
-                var key = _arr.shift(),
-                    val = {};
+    /**
+    Reverses the keys' order
+    @public
+    @method reverse
+    @chainable
+    @returns this
+    **/
+    reverse: function() {
+        this._arr.reverse();
 
-                if (_obj[key]) {
-                    val = _obj[key];
-                    delete _obj[key];
-                }
+        return this;
+    },
 
-                return val;
-            },
+    /**
+    **/
+    splice: function (index, numToRemove, elementsN) {
+        // TODO: complete splice method
+    },
 
-            unshift : function() {
-                var i = 0,
-                    l = arguments.length,
-                    key,
-                    value;
+    /**
+    Returns the position of the key in the stack
+    @public
+    @method indexOf
+    @param {String} key
+    @returns {Number}
+    **/
+    indexOf : function(key) {
+        return YArray.indexOf(this._arr, key);
+    },
 
-                for (i = 0; i < l; i++) {
-                    value = arguments[i];
+    /**
+    Returns the position of the key in the stack from the end to the front
+    @public
+    @method lastIndexOf
+    @param {String} key
+    @returns {Number}
+    **/
+    lastIndexOf: function(key) {
+        return YArray.lastIndexOf(this._arr, key);
+    },
 
-                    key = _getKey(value);
-                    if (key) {
-                        if (_keyExists(key)) {
-                            _arr = _arr.splice(_arr.indexOf(key),1);
-                        }
-                        _arr.unshift(key);
-                        _obj[key] = value[key];
-                    }
-                }
+    /**
+    Returns the number of items in the stack
+    @public
+    @method size
+    @returns {Number}
+    **/
+    size : function() {
+        return this._arr.length;
+    },
 
-                return _hash;
-            },
+    /**
+    Calls the provided function on each item in the stack
+    @public
+    @method each
+    @param {Function} fn
+        @param {mixed} fn.value Value of the current property
+        @param {String} fn.key Key of the current property
+        @param {Number} fn.index Index of the current property in the stack
+    @param {Object} [context]
+    @chainable
+    @returns this
+    **/
+    each : function(fn, context) {
 
-            reverse: function() {
-                _arr.reverse();
+        var _arr = this._arr,
+            i = 0,
+            l = _arr.length;
 
-                return _hash;
-            },
+        for (i = 0; i < l; i++) {
+            fn.call(context || this, /*value*/this._obj[_arr[i]], /*key*/_arr[i], /*index*/i);
+        }
 
-            splice: function (index, numToRemove, elementsN) {
-                // TODO: complete splice method
-            },
+        return this;
+    },
 
-            indexOf : function(key) {
-                return _arr.indexOf(key);
-            },
+    /**
+    Sets the item at the given index if provided with a number, or
+        with the given key value if a string is provided
+    @public
+    @method setItem
+    @param {String|Number} index
+    @param {Mixed} value
+    @chainable
+    @returns this
+    **/
+    setItem : function(index, value) {
+        var _arr = this._arr,
+            _obj = this._obj,
+            key;
 
-            lastIndexOf: function(key) {
-                return _arr.lastIndexOf(key);
-            },
+        if (typeof index === 'string' && Y.Object.owns(_obj, index)) {
+            _obj[index] = value;
+        } else if (_arr[parseInt(index, 10)]) {
+            index = parseInt(index, 10);
 
-            size : function() {
-                return _arr.length;
-            },
+            key = OrderedHash.getKey(value);
 
-            each : function(fn, context) {
-
-                var i = 0,
-                    l = _arr.length;
-
-                for (i = 0; i < l; i++) {
-                    fn.call(context || this, /*value*/_obj[_arr[i]], /*key*/_arr[i], /*index*/i);
-                }
-
-                return _hash;
-            },
-
-            setItem : function(index, value) {
-                if (typeof index === 'string' && _keyExists(index)) {
-                    _obj[index] = value;
-                } else if (_arr[parseInt(index, 10)]) {
-                    index = parseInt(index, 10);
-
-                    var key = _getKey(value);
-
-                    if (key) {
-                        _arr[index] = key;
-                        _obj[key] = value[key];
-                    }
-
-                } else {
-                    throw "Key is not already set. Use `push`, `unshift` or ";
-                }
-
-                return _hash;
-            },
-
-            getItem : function(index) {
-                var retObj = {};
-                if (typeof index === 'string' && _keyExists(index)) {
-                    retObj = _obj[index];
-                } else if (_arr[parseInt(index, 10)]) {
-                    retObj = _obj[_arr[parseInt(index, 10)]];
-                } else {
-                    throw "Item at index " + index + " is not found.";
-                }
-
-                return retObj;
-            },
-
-            empty: function() {
-                _arr = [];
-                _obj = {};
-
-                return _hash;
-            },
-
-            keys: function() {
-                return _arr.concat();
-            },
-
-            values: function() {
-                var i,
-                    l = _arr.length,
-                    retArr = [];
-
-                for (i=0; i<l; i++) {
-                    retArr.push(_obj[_arr[i]]);
-                }
-
-                return retArr;
-            },
-
-            pairs: function() {
-                var i,
-                    l = _arr.length,
-                    retArr = [],
-                    key;
-
-                for (i=0; i<l; i++) {
-                    key = _arr[i];
-                    retArr.push({ key: _obj[key]});
-                }
-
-                return retArr;
+            if (key) {
+                _arr[index] = key;
+                _obj[key] = value[key];
             }
 
-        };
+        } else {
+            throw "Key is not already set. Use `push`, `unshift` or ";
+        }
 
-    return _hash;
+        return this;
+    },
+
+    /**
+    Returns the item with the given key if a string is provided, or at
+        the position if a number is provided
+    @public
+    @method getItem
+    @param {String|Number} index
+    @param {Boolean} [asObject]
+    @returns {Mixed}
+    **/
+    getItem : function(index, asObject) {
+        var _arr = this._arr,
+            _obj = this._obj,
+            key;
+
+        if (typeof index === 'string' && Y.Object.owns(_obj, index)) {
+            key = index;
+        } else if (_arr[parseInt(index, 10)]) {
+            key = _arr[parseInt(index, 10)];
+        } else {
+            throw "Item at index " + index + " is not found.";
+        }
+
+        return (asObject) ? {key: _obj[key]} : _obj[key];
+    },
+
+    /**
+    Truncates the ordered hash
+    @public
+    @method empty
+    @chainable
+    @returns this
+    **/
+    empty: function() {
+        this._arr = [];
+        this._obj = {};
+
+        return this;
+    },
+
+    /**
+    Returns an array of keys in the same order of the stack
+    @public
+    @method keys
+    @returns {Array}
+    **/
+    keys: function() {
+        return this._arr.concat();
+    },
+
+    /**
+    Returns an array of values, or key:value pairs if asObject is true
+    @public
+    @method values
+    @param {Boolean} [asObject]
+    @returns {Array}
+    **/
+    values: function(asObject) {
+        var _arr = this._arr,
+            _obj = this._obj,
+            i,
+            key,
+            l = _arr.length,
+            retArr = [];
+
+        for (i=0; i<l; i++) {
+            key = _arr[i];
+            retArr.push( (asObject) ? {key: _obj[key]} : key );
+        }
+
+        return retArr;
+    }
+
 };
+
+OrderedHash.getKey = function (obj) {
+    var p;
+
+    for (p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            return p;
+        }
+    }
+};
+
+
+
+Y.OrderedHash = OrderedHash;
